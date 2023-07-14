c47e76a75300fc66f83d3b84c2156825be26725f
Karina Kokhan <58032185+Karinaaaa23@users.noreply.github.com>
2023-07-14 09:55:43 +0300

    save elements in the cart

--- src/app/cart-service/cart.service.ts
+++ src/app/cart-service/cart.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Product, products } from "../products";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, filter } from "rxjs";
+
 // import {products} from "../products";
 
 @Injectable({
@@ -11,11 +12,26 @@ export class CartService {
   //items: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>(products);
   items: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    const serializedItems = localStorage.getItem("cart");
+    if (!serializedItems) {
+      console.warn("No cart");
+      return;
+    }
+    const saveProducts = JSON.parse(serializedItems);
+    this.items.next(saveProducts);
+  }
+
+  private saveItems() {
+    console.info("adding items");
+    const storageKey = JSON.stringify(this.items.value);
+    localStorage.setItem("cart", storageKey);
+  }
 
   addToCart(product: Product): void {
     const currentData = [...this.items.getValue(), product];
     this.items.next(currentData);
+    this.saveItems();
   }
 
   getItems(): Observable<Product[]> {
@@ -27,6 +43,7 @@ export class CartService {
       (item) => item.id !== itemToRemove.id
     );
     this.items.next(value);
+    this.saveItems();
   }
 
   clearCart(): void {
